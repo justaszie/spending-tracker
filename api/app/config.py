90b49e75253c47266f08d79b5bc1e43ef0ba19f7
Justas Zieminykas <justaxsz@gmail.com>
2026-01-24 14:53:20 +0200

    JWT Feature flag: jwt validation skipped in DEV to save time

--- api/app/config.py
+++ api/app/config.py
@@ -4,14 +4,14 @@ from uuid import UUID
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class AppEnvironment(StrEnum):
-    DEVELOPMENT = "development"
-    PRODUCTION = "production"
+    DEV = "DEV"
+    PROD = "PROD"
 
 
 class AppConfig(BaseSettings):
     statements_storage_bucket: str = "statements"
     test_user_id: UUID
-    app_environment: AppEnvironment
+    app_environment: AppEnvironment = AppEnvironment.PROD
     db_connection_string: str
     supabase_url: str
     supabase_anon_key: str
