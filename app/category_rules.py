2308c3bdaf2ba54523e622c8d76d63fc70835e89
Justas Zieminykas <justaxsz@gmail.com>
2026-01-30 14:06:23 +0200

    Feat: add new category rules: shopping, gym, rent

--- app/category_rules.py
+++ app/category_rules.py
@@ -1,6 +1,7 @@
 # This module contains definition of categorization rules
+from collections.abc import Callable
 import re
-from typing import Callable, NotRequired, TypedDict
+from typing import NotRequired, TypedDict
 
 from app.project_types import ImportedTransaction
 
@@ -18,11 +19,12 @@ class CategoryData(TypedDict):
 type CategoryRuleFunction = Callable[[ImportedTransaction], CategoryData | None]
 
 # These don't need to be in lowercase anymore. Matching logic is case insensitive now
-SUPERMARKET_MERCHANTS = {"barbora", "iki", "lidl", "maxima", "rimi"}
+SUPERMARKET_MERCHANTS = {"barbora", "iki", "lidl", "maxima", "rimi", "narvesen"}
 COFFESHOP_MERCHANTS = {
     "caffeine",
     "kavos era",
     "brew. specialty coffee",
+    "baristokrat specialty coffee",
     "albas",
     "backstage cafe",
     "caif cafe",
@@ -39,7 +41,9 @@ BUSINESS_LUNCH_MERCHANTS = {
     "bernelių užeiga",
     "Ministerija Dienos pietūs",
     "A. Taraškienės firma 3515",
+    "Senolių tradicija",
 }
+
 STREAMING_MERCHANTS = {"disney", "netflix", "spotify", "youtube"}
 FOOD_DELIVERY_MERCHANTS = {"bolt food", "wolt"}
 RESTAURANT_MERCHANTS = {
@@ -61,9 +65,27 @@ RESTAURANT_MERCHANTS = {
     "Jūsų Šnekutis",
 }
 
+ECOM_MERCHANTS = (
+    r".*amazon(?:\s.*)?$",
+    r"pigu(?:\.|\s)lt",
+    r"varle",
+)
+ECOM_MERCHANT_PATTERNS = tuple(
+    re.compile(pattern, re.IGNORECASE) for pattern in ECOM_MERCHANTS
+)
+
+CLOTHES_SHOPPING_MERCHANTS = (r"zalando", r"zara", r"h&m")
+CLOTHES_SHOPPING_PATTERNS = tuple(
+    re.compile(pattern, re.IGNORECASE) for pattern in CLOTHES_SHOPPING_MERCHANTS
+)
+
+GYM_MEMBERSHIP_PATTERNS = tuple(
+    re.compile(pattern, re.IGNORECASE) for pattern in (r'lemon gym', )
+)
+
 
 ### RULE DEFINITIONS
-def _is_eating_out(transaction: ImportedTransaction) -> CategoryData | None:
+def eating_out(transaction: ImportedTransaction) -> CategoryData | None:
     counterparty = transaction.counterparty.lower().strip()
     if any(merchant.lower() in counterparty for merchant in RESTAURANT_MERCHANTS):
         return {
@@ -74,7 +96,7 @@ def _is_eating_out(transaction: ImportedTransaction) -> CategoryData | None:
     return None
 
 
-def _is_food_delivery(transaction: ImportedTransaction) -> CategoryData | None:
+def food_delivery(transaction: ImportedTransaction) -> CategoryData | None:
     counterparty = transaction.counterparty.lower().strip()
     if any(counterparty == merchant.lower() for merchant in FOOD_DELIVERY_MERCHANTS):
         return {
@@ -85,7 +107,7 @@ def _is_food_delivery(transaction: ImportedTransaction) -> CategoryData | None:
     return None
 
 
-def _is_business_lunch(transaction: ImportedTransaction) -> CategoryData | None:
+def business_lunch(transaction: ImportedTransaction) -> CategoryData | None:
     counterparty = transaction.counterparty.lower().strip()
     if (
         any(counterparty == merchant.lower() for merchant in BUSINESS_LUNCH_MERCHANTS)
@@ -101,7 +123,7 @@ def _is_business_lunch(transaction: ImportedTransaction) -> CategoryData | None:
     return None
 
 
-def _is_streaming_services(transaction: ImportedTransaction) -> CategoryData | None:
+def streaming_services(transaction: ImportedTransaction) -> CategoryData | None:
     counterparty = transaction.counterparty.lower().strip()
     if any(
         re.search(rf"^{merchant}.*$", counterparty, flags=re.IGNORECASE)
@@ -115,7 +137,7 @@ def _is_streaming_services(transaction: ImportedTransaction) -> CategoryData | N
     return None
 
 
-def _is_groceries(transaction: ImportedTransaction) -> CategoryData | None:
+def groceries(transaction: ImportedTransaction) -> CategoryData | None:
     counterparty = transaction.counterparty.lower().strip()
     if any(counterparty == merchant.lower() for merchant in SUPERMARKET_MERCHANTS):
         return {
@@ -126,7 +148,7 @@ def _is_groceries(transaction: ImportedTransaction) -> CategoryData | None:
     return None
 
 
-def _is_breakfast(transaction: ImportedTransaction) -> CategoryData | None:
+def breakfast(transaction: ImportedTransaction) -> CategoryData | None:
     counterparty = transaction.counterparty.lower().strip()
     if (
         counterparty in ("caffeine", "kavos era")
@@ -141,7 +163,7 @@ def _is_breakfast(transaction: ImportedTransaction) -> CategoryData | None:
     return None
 
 
-def _is_hot_drinks(transaction: ImportedTransaction) -> CategoryData | None:
+def hot_drinks(transaction: ImportedTransaction) -> CategoryData | None:
     counterparty = transaction.counterparty.lower().strip()
     if (
         any(counterparty == merchant.lower() for merchant in COFFESHOP_MERCHANTS)
@@ -155,13 +177,75 @@ def _is_hot_drinks(transaction: ImportedTransaction) -> CategoryData | None:
     return None
 
 
+def landlord_payment(transaction: ImportedTransaction) -> CategoryData | None:
+    to_landlord = transaction.counterparty.upper() == "AUŠRA ADELĖ VAIŠVILĖ"
+    if not to_landlord:
+        return None
+
+    if transaction.orig_amount >= 400:
+        return {
+            "l1_category": "Rent",
+            "l2_category": "Rent",
+            "l3_category": "Rent",
+        }
+
+    return {
+        "l1_category": "Rent",
+        "l2_category": "Utilities",
+        "l3_category": "Utilities",
+    }
+
+
+def shopping_clothes(transaction: ImportedTransaction) -> CategoryData | None:
+    if any(
+        pattern.search(transaction.counterparty) is not None
+        for pattern in CLOTHES_SHOPPING_PATTERNS
+    ):
+        return {
+            "l1_category": "Shopping",
+            "l2_category": "Clothes",
+            "l3_category": "Clothes",
+        }
+
+    return None
+
+
+def shopping_other(transaction: ImportedTransaction) -> CategoryData | None:
+    if any(
+        pattern.search(transaction.counterparty) is not None
+        for pattern in ECOM_MERCHANT_PATTERNS
+    ):
+        return {
+            "l1_category": "Shopping",
+        }
+
+    return None
+
+def gym_membership(transaction: ImportedTransaction) -> CategoryData | None:
+    if any(
+        pattern.search(transaction.counterparty) is not None
+        for pattern in GYM_MEMBERSHIP_PATTERNS
+    ):
+        return {
+            "l1_category": "Health",
+            "l2_category": "Gym",
+            "l3_category": "Gym",
+        }
+
+    return None
+
+
 # For now, the list of specific categorization rules is setup manually
 CATEGORY_RULES: list[CategoryRuleFunction] = [
-    _is_streaming_services,
-    _is_groceries,
-    _is_breakfast,
-    _is_hot_drinks,
-    _is_business_lunch,
-    _is_eating_out,
-    _is_food_delivery,
+    streaming_services,
+    groceries,
+    breakfast,
+    hot_drinks,
+    business_lunch,
+    eating_out,
+    food_delivery,
+    landlord_payment,
+    shopping_clothes,
+    shopping_other,
+    gym_membership,
 ]
