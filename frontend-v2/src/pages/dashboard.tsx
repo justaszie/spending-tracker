6ab1704c3bc4a8024af942b1d9532de5188b44f1
Justas Zieminykas <justaxsz@gmail.com>
2026-03-11 14:41:58 +0200

    Remove todo comment from dashboard

--- frontend-v2/src/pages/dashboard.tsx
+++ frontend-v2/src/pages/dashboard.tsx
@@ -96,7 +96,6 @@ export default function Dashboard() {
   }
 
   const transactions = data?.transactions ?? [];
-  // TODO - Remove the test value once the API returns proper total value
   const totalCount = data?.total || 0;
   const spendingCategories = spendingCategoriesData ?? [];
 
